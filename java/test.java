6198ff46c12714e6765c2f8584761e6774c3531a
yhpark <yhpark>
2025-04-21 11:17:58 +0900

    [ + ] 자바 프로그램 실습 파일

---
+++ java/test.java
@@ -0,0 +1,10 @@
+public class test {
+	public static void show() {
+		System.out.println("University of Yuan Computer Science & Engineering");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("나는 유한대 학생입니다");
+		show();
+	}
+}
