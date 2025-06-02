3c8cf4d5a8ae6e0aed11123a9bb707dcecb56446
yhpark <zeroreturn93@yuhan.ac.kr>
2025-06-02 11:50:33 +0900

    Signed-off-by: yhpark <zeroreturn93@yuhan.ac.kr>

--- java work/JavaStudy71/src/temporary/dog.java
+++ java work/JavaStudy71/src/temporary/dog.java
@@ -4,29 +4,11 @@ import oop.student;
 
 public class dog
 {
-//	public String breed;
-//	public String color;
-//	protected int age;
-//	private String name;
-//
-//	public void bowwow()
-//	{
-//		System.out.printf("멍멍 짖다");
-//	}
-//
-//	public void run()
-//	{
-//		System.out.printf("달리다");
-//	}
-//
-//	public void sleep()
-//	{
-//		System.out.printf("낮잠 잔다");
-//	}
-	
-	String breed;
-	String color;
-	
+	public String breed;
+	public String color;
+	protected int age;
+	private String name;
+
 	public void bowwow()
 	{
 		System.out.printf("멍멍 짖다");
@@ -41,5 +23,23 @@ public class dog
 	{
 		System.out.printf("낮잠 잔다");
 	}
+	
+//	String breed;
+//	String color;
+//	
+//	public void bowwow()
+//	{
+//		System.out.printf("멍멍 짖다");
+//	}
+//
+//	public void run()
+//	{
+//		System.out.printf("달리다");
+//	}
+//
+//	public void sleep()
+//	{
+//		System.out.printf("낮잠 잔다");
+//	}
 	
 }
---
+++ java work/JavaStudy71/src/temporary/ex3333.java
@@ -0,0 +1,15 @@
+package temporary;
+
+public class ex3333
+{	
+	public static void main(String[] args)
+	{
+		persion1 p1 = new persion1();
+		
+		System.out.println(p1.getname());		
+		System.out.println(p1.getage());		
+		System.out.println(p1.getaddress());
+		
+		
+	}
+}
