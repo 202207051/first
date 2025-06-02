2d89ebb9bda2fba09ef638f36cffd481231dfa3b
yhpark <zeroreturn93@yuhan.ac.kr>
2025-06-02 10:56:48 +0900

    3교시 실습내용

---
+++ java work/JavaStudy71/src/temporary/dog.java
@@ -0,0 +1,45 @@
+package temporary;
+
+import oop.student;
+
+public class dog
+{
+//	public String breed;
+//	public String color;
+//	protected int age;
+//	private String name;
+//
+//	public void bowwow()
+//	{
+//		System.out.printf("멍멍 짖다");
+//	}
+//
+//	public void run()
+//	{
+//		System.out.printf("달리다");
+//	}
+//
+//	public void sleep()
+//	{
+//		System.out.printf("낮잠 잔다");
+//	}
+	
+	String breed;
+	String color;
+	
+	public void bowwow()
+	{
+		System.out.printf("멍멍 짖다");
+	}
+
+	public void run()
+	{
+		System.out.printf("달리다");
+	}
+
+	public void sleep()
+	{
+		System.out.printf("낮잠 잔다");
+	}
+	
+}
