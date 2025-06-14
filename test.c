8cd6f9554bdfe6189c9db623bed6a9095336d269
yuhark <zeroreturn93@yuhan.ac.kr>
2025-06-14 14:55:37 +0900

    add leacture

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 1;
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d\t", num);
+            num++;
+        }
+        printf("\n");
+    }
+}
