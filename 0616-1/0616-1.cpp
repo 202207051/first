1952202ac40139350640f8efeced523d8f2d29db
yhpark <zeroreturn93@yuhan.ac.kr>
2025-06-16 09:09:13 +0900

    add codes

---
+++ 0616-1/0616-1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int mul(int, int);
+int add(int, int);
+
+int main()
+{
+	int a = 4;
+	int b = 2;
+	int result;
+
+	result = mul(a, b);
+
+	printf("mul : %d\n", result);
+}
+int mul(int a, int b)
+{
+	int sum = 0;
+
+	for (int i = 0; i < b; i++)
+	{
+		sum = add(sum, a);
+	}
+
+	return sum;
+}
+
+int add(int a, int b)
+{
+	return a + b;
+}
