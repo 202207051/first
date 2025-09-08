ba448537e5b298b07027f08661d27e10f4fd3f93
yhpark <zeroreturn93@yuhan.ac.kr>
2025-09-08 15:24:24 +0900

    remvoe javaproject

--- eclipse-workspace/javastudy1/src/code99.java
+++ eclipse-workspace/javastudy1/src/code99.java
@@ -1,23 +1,23 @@
-class friend1
-{
-	String name;
-	int age;
-
-	void printfriend()
-	{
-		System.out.println("name : " + name + ", age : " + age);
+class BankAccount {
+	String name; // 고객명
+	double balance; // 계좌 잔고
+	static int count; // 고객 수
+	BankAccount(String name, double balance) {
+		this.name = name;
+		this.balance = balance;
+		count ++;
+		System.out.println(name + " 고객의 잔고 : " + balance);
 	}
 }
+public class code99 {
+	public static void main(String[] args) {
+		BankAccount ba1 = new BankAccount("Alice", 100000);
+		BankAccount ba2 = new BankAccount("Paul", 50000);
+		BankAccount ba3 = new BankAccount("Cindy", 30000);
+		BankAccount ba4 = new BankAccount("David", 150000);
+		BankAccount ba5 = new BankAccount("Tom", 200000);
 
-public class code99
-{
-	public static void main(String[] args)
-	{
-		friend1 f = new friend1();
-		f.printfriend();
-
-		f.name = "alce";
-		f.age = 20;
-		f.printfriend();
+		System.out.println();
+		System.out.println("number of accounts : " + BankAccount.count);
 	}
 }
